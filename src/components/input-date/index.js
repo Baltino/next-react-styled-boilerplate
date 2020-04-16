a9884e669e7be7a5ef30011c31ed4fe36722b528
Maxi Aragon <jose.maximiliano.aragon@gmail.com>
2020-04-16 20:23:31 -0300

    range calendar

--- src/components/input-date/index.js
+++ src/components/input-date/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { array, func, number, bool, string, oneOfType } from "prop-types";
 import Calendar from "react-calendar";
 import styled from "styled-components";
 import { Flex } from "rebass/styled-components";
@@ -7,30 +8,53 @@ import moment from "moment";
 import "react-calendar/dist/Calendar.css";
 import Input from "../input";
 
+const propTypes = {
+  open: bool,
+  placeholder: string,
+  format: string,
+  onChange: func,
+  selectRange: bool,
+  width: oneOfType([array, string, number])
+};
+
 const CalendarContainer = styled(Flex)`
   position: absolute;
   top: 50px;
   display: ${({ isOpen }) => isOpen ? "block" : "none"};
 `;
 
-const InputDate = () => {
-  const [isOpen, setIsOpen] = useState(false);
+const InputDate = ({ 
+  open, 
+  placeholder = "", 
+  format = "MM-DD-YYYY", 
+  onChange = () => {}, 
+  selectRange = false,
+  width = 300,
+}) => {
+  const [isOpen, setIsOpen] = useState(open);
   const [value, setValue] = useState()
-  const handleClick = () => setIsOpen(!isOpen);
+  const handleClick = () => setIsOpen(!isOpen)
   const handleChange = value => {
     setIsOpen(false);
     setValue(value);
+    onChange(value);
   };
-  const formated = value ? moment(value).format("DD-MM-YYYY") : "";
+  const formated = value ? 
+    (selectRange ? 
+      `${moment(value[0]).format(format)} - ${moment(value[1]).format(format)}` : moment(value).format(format)
+    )
+    : "";
   return (
     <Flex sx={{ position: "relative" }}>
-      <Input onClick={handleClick} value={formated} placeholder={"Inicio"} width={300} readOnly/>
-      <CalendarContainer width={300} isOpen={isOpen}>
-        <Calendar onChange={handleChange} value={value} />
+      <Input onClick={handleClick} value={formated} placeholder={placeholder} width={width} readOnly/>
+      <CalendarContainer width={width} isOpen={isOpen}>
+        <Calendar onChange={handleChange} value={value} selectRange={selectRange} />
       </CalendarContainer>
     </Flex>
   );
 
 }
+
+InputDate.propTypes = propTypes;
 
 export default InputDate;
--- src/components/input-date/index.stories.js
+++ src/components/input-date/index.stories.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import { Flex } from "rebass/styled-components";
 
 import InputDate from "./index";
 
 export default { title: "Input Date" };
 
-export const Simple = () => <InputDate />;
+export const Simple = () => <InputDate placeholder={"Simple date"} />;
+
+export const Range = () => {
+  const [dates, setDates] = useState();
+  const FORMAT = "DD-MM-YYYY";
+  const handleOnChange = value => setDates(value)
+  console.log(dates)
+  return (
+    <Flex flexDirection={"column"}>
+      <Flex>
+        <InputDate placeholder="Inicio" format={FORMAT} selectRange={true} onChange={handleOnChange} />
+      </Flex>
+    </Flex>
+  );
+}
