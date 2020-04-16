33a2fb52f92030082def3f85e98dbf14f1957b24
Maxi Aragon <jose.maximiliano.aragon@gmail.com>
2020-04-15 23:43:01 -0300

    input date placeholder

---
+++ src/components/input-date/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const InputDate = () => <div>ToDo</div>;
+
+export default InputDate;
---
+++ src/components/input-date/index.stories.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import InputDate from "./index";
+
+export default { title: "Input Date" };
+
+export const Simple = () => <InputDate />;
